610de9cd4d255ad7036c1f3a7e2c01cf8367c0bd
wlisses <wlissesfernando285@gmail.com>
2024-03-22 22:28:40 -0300

    changed formatter

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -19,15 +19,6 @@ function Provider({ children }) {
     is: false,
   });
 
-  function clearInputs() {
-    document.getElementById("input_title").value = "";
-    document.getElementById("input_music").value = "";
-    document.getElementById("input_image").value = "";
-    document.getElementById("input_category").value = "";
-    document.getElementById("input_description").value = "";
-  }
-
-
   function handleChange({ target }) {
     const { name, value } = target;
     setFormData({ ...formData, [name]: value });
@@ -99,7 +90,9 @@ function Provider({ children }) {
     alert(response.message);
     clearData();
     setIsUpdate({ music: {}, is: false });
-    clearInputs();
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
   }
 
   useEffect(() => {
